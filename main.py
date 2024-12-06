0d130f70615e788530d15af10239bf07529b93e7
jjboykin <jeremy.j.boykin@gmail.com>
2024-12-06 16:53:08 -0500

    player sprite

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main ():
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Display Mode: {SCREEN_WIDTH}x{SCREEN_HEIGHT}")
 
     # Initialize Pygame
     pygame.init()
+    
     # Set up the display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Set up the display
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Set up the player
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
     # Game loop
     running = True
     while running:
@@ -20,14 +26,19 @@ def main ():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         # Game logic (update game state, etc.)
-        # ...
+        
 
         # Drawing (render graphics)
         screen.fill("black")
+        player.draw(screen)
 
         # Update the display
-        pygame.display.flip()    
+        pygame.display.flip()
+        
+        # Limit the framerate
+        dt = clock.tick(SCREEN_FPS) / CONVERT_MILLISECONDS_TO_SECONDS    
         
     # Quit Pygame
     pygame.quit()       
